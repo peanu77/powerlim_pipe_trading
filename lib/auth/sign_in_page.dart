5bd974b96f3a57f8a7dd3b44af3d446a0886d71d
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2022-12-11 00:25:46 +0800

    refs #4 initial sign in page closes #4

---
+++ lib/auth/sign_in_page.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const SizedBox(height: 40),
+        TextField(
+          controller: emailController,
+          cursorColor: Colors.white,
+          textInputAction: TextInputAction.next,
+          decoration: const InputDecoration(labelText: 'Email'),
+        ),
+        const SizedBox(height: 4),
+        TextField(
+          controller: passwordController,
+          textInputAction: TextInputAction.done,
+          decoration: const InputDecoration(labelText: 'Password'),
+          obscureText: true,
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton.icon(
+          style:
+              ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50)),
+          icon: const Icon(Icons.lock_open, size: 32),
+          label: const Text(
+            'Sign In',
+            style: TextStyle(fontSize: 24),
+          ),
+          onPressed: () => signIn(),
+        )
+      ]),
+    );
+  }
+
+  Future signIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim());
+  }
+}
