e12d4686c31ca78e38798ea12d8cdbeec2cfc21a
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2022-12-11 03:12:27 +0800

    refs #4 added sign in page closes  #4

--- lib/auth/sign_in_page.dart
+++ lib/auth/sign_in_page.dart
@@ -1,5 +1,8 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:powerlim_pipe_trading/auth/sign_up_page.dart';
+import 'package:powerlim_pipe_trading/main.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -12,6 +15,8 @@ class _SignInPageState extends State<SignInPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  bool isShow = true;
+
   @override
   void dispose() {
     emailController.dispose();
@@ -22,41 +27,97 @@ class _SignInPageState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const SizedBox(height: 40),
-        TextField(
-          controller: emailController,
-          cursorColor: Colors.white,
-          textInputAction: TextInputAction.next,
-          decoration: const InputDecoration(labelText: 'Email'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 40),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: emailController,
+              cursorColor: Colors.black,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(labelText: "Email"),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: (email) =>
+                  email != null && !EmailValidator.validate(email)
+                      ? "Enter a valid email"
+                      : null,
+            ),
+            const SizedBox(height: 4),
+            TextFormField(
+              controller: passwordController,
+              cursorColor: Colors.black,
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(
+                labelText: "Password",
+                suffixIcon: IconButton(
+                  onPressed: () => setState(() => isShow = !isShow),
+                  icon: isShow
+                      ? const Icon(Icons.visibility_off)
+                      : const Icon(Icons.visibility_outlined),
+                ),
+              ),
+              obscureText: isShow,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              // validator: (value) => value != null && value.length < 6
+              //     ? "Enter min of 6 charters"
+              //     : null,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Enter a password";
+                }
+                if (value.length < 6) {
+                  return "Password must be atleast 6 characters";
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50)),
+              icon: const Icon(Icons.lock_open, size: 32),
+              label: const Text(
+                'Sign In',
+                style: TextStyle(fontSize: 24),
+              ),
+              onPressed: () => signIn(),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const SignUpPage(),
+                  ),
+                );
+              },
+              child: const Text('Sign Up'),
+            )
+          ],
         ),
-        const SizedBox(height: 4),
-        TextField(
-          controller: passwordController,
-          textInputAction: TextInputAction.done,
-          decoration: const InputDecoration(labelText: 'Password'),
-          obscureText: true,
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton.icon(
-          style:
-              ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50)),
-          icon: const Icon(Icons.lock_open, size: 32),
-          label: const Text(
-            'Sign In',
-            style: TextStyle(fontSize: 24),
-          ),
-          onPressed: () => signIn(),
-        )
-      ]),
+      ),
     );
   }
 
   Future signIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text.trim(),
-        password: passwordController.text.trim());
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => const Center(
+              child: CircularProgressIndicator(),
+            ));
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text.trim(),
+          password: passwordController.text.trim());
+    } on FirebaseAuthException catch (e) {
+      print(e);
+    }
+    navigatorKey.currentState!.popUntil((route) => route.isFirst);
   }
 }
